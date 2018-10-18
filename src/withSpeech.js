b6fa193d043790904f4cd89f0652567287de1ec2
Ademola Adegbuyi <ooade96@gmail.com>
2018-10-18 23:27:06 +0100

    Add Speech

---
+++ src/withSpeech.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const Speech = React.createContext()
+
+export class SpeechProvider extends React.Component {
+	speak = async text => {
+		const msg = new SpeechSynthesisUtterance(text)
+		msg.voice = speechSynthesis.getVoices()[3]
+		speechSynthesis.speak(msg)
+	}
+
+	UNSAFE_componentWillMount() {
+		if (typeof window !== 'undefined') {
+			speechSynthesis.getVoices()
+		}
+	}
+
+	componentDidMount() {
+		if (!('speechSynthesis' in window)) {
+			console.log('Browser not supported')
+		}
+	}
+
+	render() {
+		return (
+			<Speech.Provider value={{ speak: this.speak }}>
+				{this.props.children}
+			</Speech.Provider>
+		)
+	}
+}
+
+export default ComposedComponent => props => (
+	<Speech.Consumer>
+		{value => <ComposedComponent speech={value} {...props} />}
+	</Speech.Consumer>
+)
